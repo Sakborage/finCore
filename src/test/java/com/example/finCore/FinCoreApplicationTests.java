595dd20758e615b8c069daf8e27cc26bf87ff802
Sakborage <sakshiborage19@gmail.com>
2026-02-03 12:34:24 +0530

    Initial Setup

---
+++ src/test/java/com/example/finCore/FinCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
